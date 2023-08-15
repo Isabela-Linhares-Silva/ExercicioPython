42519cb44ca3ca10bed63af087bb852d4170a4f9
Isabela-Linhares-Silva <140658898+Isabela-Linhares-Silva@users.noreply.github.com>
2023-08-15 16:47:54 -0300

    Add files via upload

---
+++ Mundo1/desafio007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Nota 1:'))
+n2 = float(input('Nota 2:'))
+s = n1 + n2
+m = s / 2
+print("A média do aluno foi {}".format(m))
---
+++ Mundo1/desafio009.py
@@ -0,0 +1,12 @@
+z1=int(input('Digite um valor:'))
+a = z1 * 1
+b = z1 * 2
+c = z1 * 3
+d = z1 * 4
+e = z1 * 5
+f = z1 * 6
+g = z1 * 7
+h = z1 * 8
+i = z1 * 9
+j = z1 * 10
+print('A tabuada desse número é:{}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {} '.format(a, b, c, d, e, f, g, h, i, j,))
